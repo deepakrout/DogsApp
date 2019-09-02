a26fe2c81da5c7a6cbfa3d7dcc3f0742377e27ee
Deepak Rout <deepakrout@gmail.com>
2019-09-01 22:33:04 -0400

    Added adapater, ViewModel and the Model

--- app/src/main/java/com/iappstogo/dogsapp/view/ListFragment.kt
+++ app/src/main/java/com/iappstogo/dogsapp/view/ListFragment.kt
@@ -6,16 +6,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.NavDirections
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.iappstogo.dogsapp.R
+import com.iappstogo.dogsapp.viewmodel.ListViewModel
 import kotlinx.android.synthetic.main.fragment_list.*
 import java.util.*
 
 
 //Fragments: List Fragment
 class ListFragment : Fragment() {
+
+    private lateinit var viewModel: ListViewModel
+    private val dogsListAdapter = DogsListAdapter(arrayListOf())
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +33,45 @@ class ListFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_list, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
+        viewModel.refresh()
+
+        dogsList.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = dogsListAdapter
+        }
+
+        observeViewModel()
+    }
+
+    private fun observeViewModel() {
+        viewModel.dogs.observe(this, androidx.lifecycle.Observer{ dogs ->
+            dogs?.let {
+                dogsList.visibility = View.VISIBLE
+                dogsListAdapter.updateDogList(dogs)
+            }
+        })
+
+        viewModel.dogsLoadError.observe(this, androidx.lifecycle.Observer { isError->
+            isError?.let {
+                listError.visibility = if (it) View.VISIBLE else View.GONE
+            }
+        })
+
+        viewModel.loading.observe(this, androidx.lifecycle.Observer { isLoading->
+            isLoading?.let {
+                loadingView.visibility = if (it) View.VISIBLE else View.GONE
+                if (it) {
+                    listError.visibility = View.GONE
+                    dogsList.visibility = View.GONE
+                }
+            }
+
+        })
+    }
 
 
 }
---
+++ app/src/main/java/com/iappstogo/dogsapp/viewmodel/ListViewModel.kt
@@ -0,0 +1,23 @@
+package com.iappstogo.dogsapp.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.iappstogo.dogsapp.model.DogBreed
+
+class ListViewModel(): ViewModel() {
+    val dogs = MutableLiveData<List<DogBreed>>()
+    val dogsLoadError = MutableLiveData<Boolean>()
+    val loading = MutableLiveData<Boolean>()
+
+    fun refresh() {
+        val dog1 = DogBreed("1","Corgi", "15 years", "breedGroup","BreFor", "Temerament","")
+        val dog2 = DogBreed("2","Labrador", "15 years", "breedGroup","BreFor", "Temerament","")
+        val dog3 = DogBreed("3","Rotwailer", "20 years", "breedGroup","BreFor", "Temerament","")
+        val dogList: ArrayList<DogBreed> = arrayListOf<DogBreed>(dog1, dog2, dog3)
+
+        dogs.value = dogList
+        dogsLoadError.value = false
+        loading.value = false
+
+    }
+}
