f7f5dd772a714be8897b599b5f3629aff727eb91
Deepak Rout <deepakrout@gmail.com>
2019-09-07 23:24:01 -0400

    Added adapater, ViewModel and the Model

--- app/src/main/java/com/iappstogo/dogsapp/view/DogsListAdapter.kt
+++ app/src/main/java/com/iappstogo/dogsapp/view/DogsListAdapter.kt
@@ -26,6 +26,7 @@ class DogsListAdapter(val dogsList: ArrayList<DogBreed>) : RecyclerView.Adapter<
     //Get item counts
     override fun getItemCount() = dogsList.size
 
+    //Bind view holder
     override fun onBindViewHolder(holder: DogViewHolder, position: Int) {
 
         holder.view.name.text = dogsList[position].dogBreed
